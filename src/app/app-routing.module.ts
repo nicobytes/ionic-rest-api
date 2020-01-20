95c4aafc33a953e0078178455071ae8a658c987e
GermanCode <w.aguirre@udla.edu.co>
2020-01-20 00:28:51 -0500

    se agrega la funcionalidad de update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'update', loadChildren: './update/update.module#UpdatePageModule' },
+
 ];
 
 @NgModule({
